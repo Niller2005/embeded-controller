b9eb3311b2829e5e0718238674af342dc547e67e
Jacob Nielsen <jacob@sitetech.dk>
2019-01-09 15:34:54 +0100

    new pause

--- src/main.cpp
+++ src/main.cpp
@@ -6,59 +6,90 @@
 LCD_DISCO_F746NG lcd;
 
 DigitalOut led1(LED1);
+DigitalOut led2(D2);
 InterruptIn b1(USER_BUTTON);
 InterruptIn b2(D4);
 Serial pc(USBTX, USBRX);
 
 bool running = true;
-bool clicked = false;
+bool paused = false;
+
 int i = 0;
 
 void pause()
 {
-	running = !running;
-
-	pc.printf("\r\n----- Counter Paused -----\r\n");
-}
-
-void reset()
-{
-	i = 0;
-	lcd.Clear(LCD_COLOR_BLUE);
+	if (!paused)
+	{
+		paused = true;
+		lcd.Clear(LCD_COLOR_LIGHTCYAN);
+		lcd.SetBackColor(LCD_COLOR_LIGHTCYAN);
+		lcd.SetTextColor(LCD_COLOR_BLACK);
+		pc.printf("\r\n----- Counter Paused -----\r\n");
+	}
+	else if (paused)
+	{
+		running = true;
+		paused = false;
+		lcd.Clear(LCD_COLOR_BLUE);
+		lcd.SetBackColor(LCD_COLOR_BLUE);
+		lcd.SetTextColor(LCD_COLOR_WHITE);
+		pc.printf("\r\n----- Counter Resmumed -----\r\n");
+	}
 }
 
 int main()
 {
-	b1.rise(&pause);
-	b2.rise(&pause);
 
+	// Start screen
 	lcd.Clear(LCD_COLOR_BLUE);
 	lcd.SetBackColor(LCD_COLOR_BLUE);
 	lcd.SetTextColor(LCD_COLOR_WHITE);
 	lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"Let the count begin!", CENTER_MODE);
+
 	wait(1);
 	lcd.Clear(LCD_COLOR_BLUE);
+	lcd.SetBackColor(LCD_COLOR_BLUE);
+	lcd.SetTextColor(LCD_COLOR_WHITE);
 
-	uint32_t count;
+	b2.rise(&pause);
+	b1.rise(&pause);
+	uint32_t count = 0;
+	char screen_int[32];
+
 	while (1)
 	{
-
 		if (running)
 		{
-			count = i++;
+			count++;
+			if (count >= 10000)
+			{
+				count = 0;
+				lcd.Clear(LCD_COLOR_BLUE);
+			}
+			sprintf(screen_int, "Count: %lu", count);
 		}
-
-		char screen_int[32];
-		sprintf(screen_int, "Count: %lu", count);
-
-		if (count >= 10000)
-		{
-			reset();
-		}
-		else if (running)
+		if (!paused)
 		{
 			pc.printf("Count: %d\r\n", count);
-			lcd.DisplayStringAt(0, LINE(5), (uint8_t *)&screen_int, CENTER_MODE);
+			lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"              ", CENTER_MODE);
+			lcd.DisplayStringAt(0, LINE(5), (uint8_t *)screen_int, CENTER_MODE);
+			led2 = 1;
+
+			// Onboard LED blink every 10 number
+			if (count % 10 == 0)
+				led1 = 1;
+			else
+				led1 = 0;
+
+			running = true;
+		}
+		else
+		{
+			lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"--- Paused ---", CENTER_MODE);
+			lcd.DisplayStringAt(0, LINE(5), (uint8_t *)screen_int, CENTER_MODE);
+			led2 = 0;
+
+			running = false;
 		}
 	}
 }
