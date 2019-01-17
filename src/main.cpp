e87a46f1ac1b352cbfb3b7241171d531a6476a71
Jacob Nielsen <jacob@sitetech.dk>
2019-01-17 12:38:10 +0100

    threads + events

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include "mbed.h"
+#include "rtos.h"
+#include "DHT.h"
 #include "LCD_DISCO_F746NG.h"
 #include <iostream>
-#include <string>
 
 LCD_DISCO_F746NG lcd;
 
+InterruptIn btn(D4);
 DigitalOut led1(LED1);
 DigitalOut led2(D2);
-InterruptIn b1(USER_BUTTON);
-InterruptIn b2(D4);
 Serial pc(USBTX, USBRX);
 
-bool running = true;
-bool paused = false;
+Thread counter;
+EventQueue queue;
 
-int i = 0;
+
+volatile bool running = true;
+bool paused = false;
 
 void pause()
 {
@@ -73,15 +75,8 @@ void start_count()
 	lcd.SetTextColor(LCD_COLOR_WHITE);
 }
 
-int main()
+void counter_thread()
 {
-
-	// Start screen
-	boot();
-	start_count();
-
-	b2.rise(&pause);
-	b1.rise(&pause);
 	uint32_t count = 0;
 	char screen_int[32];
 
@@ -98,13 +93,12 @@ int main()
 		}
 		if (!paused)
 		{
-			pc.printf("Count: %04d\r\n", count);
 			lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"              ", CENTER_MODE);
 			lcd.DisplayStringAt(0, LINE(5), (uint8_t *)screen_int, CENTER_MODE);
 			led2 = 1;
 
 			// Onboard LED blink every 10 number
-			if (count % 10 == 0)
+			if (count % 100 == 0)
 				led1 = 1;
 			else
 				led1 = 0;
@@ -119,5 +113,23 @@ int main()
 
 			running = false;
 		}
+	}
+}
+
+
+
+int main()
+{
+	Thread eventThread;
+	eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
+	
+	btn.fall(queue.event(&pause));
+	// Start screen
+	boot();
+	start_count();
+	counter.start(counter_thread);
+
+	while (1)
+	{
 	}
 }
