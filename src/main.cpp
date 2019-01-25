e29317a5035662014b06771a3de7200375ffbf84
Jacob Nielsen <jacob@sitetech.dk>
2019-01-25 08:32:45 +0100

    light sensor + user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,66 @@
 #include "mbed.h"
-#include "rtos.h"
 #include "DHT.h"
 #include "LCD_DISCO_F746NG.h"
 #include <iostream>
 
 LCD_DISCO_F746NG lcd;
 
-InterruptIn btn(D4);
+InterruptIn btn(USER_BUTTON);
 DigitalOut led1(LED1);
 DigitalOut led2(D2);
+AnalogIn ls(A0);
+DHT sensor(A1, SEN51035P);
+
 Serial pc(USBTX, USBRX);
 
 Thread counter;
+Thread ths;
+Thread light;
+// create an event queue
 EventQueue queue;
 
+char br[64];
 
-volatile bool running = true;
-bool paused = false;
+eScale corf = CELCIUS;
+char temp = 'C';
 
-void pause()
+void temp_switch()
 {
-	if (!paused)
+	// this now runs in the context of eventThread, instead of in the ISR
+	led2 = !led2;
+	if (!led2)
 	{
-		paused = true;
-		lcd.Clear(LCD_COLOR_LIGHTCYAN);
-		lcd.SetBackColor(LCD_COLOR_LIGHTCYAN);
-		lcd.SetTextColor(LCD_COLOR_BLACK);
-		pc.printf("\r\n----- Counter Paused -----\r\n");
+		corf = CELCIUS;
+		temp = 'C';
+		pc.printf("Switched to Celcius\r\n");
 	}
-	else if (paused)
+	else
 	{
-		running = true;
-		paused = false;
-		lcd.Clear(LCD_COLOR_BLUE);
-		lcd.SetBackColor(LCD_COLOR_BLUE);
-		lcd.SetTextColor(LCD_COLOR_WHITE);
-		pc.printf("\r\n----- Counter Resmumed -----\r\n");
+		corf = FARENHEIT;
+		temp = 'F';
+		pc.printf("Switched to Farenheit\r\n");
 	}
+}
+
+void user_input()
+{
+	// char buffer[128];
+	char b[8];
+	char r[8];
+
+	lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"Waiting on input..", CENTER_MODE);
+	pc.printf("Indtast bygnings nummer: ");
+	pc.scanf("%s", b);
+	pc.printf("%s\n", b);
+
+	pc.printf("Indtast lokale nummer: ");
+	pc.scanf("%s", r);
+	pc.printf("%s\n", r);
+
+	sprintf(br, "Bygning: %s - Lokale: %s", b, r);
+
+	lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"                  ", CENTER_MODE);
+	lcd.DisplayStringAt(0, LINE(1), (uint8_t *)br, CENTER_MODE);
 }
 
 void boot()
@@ -61,75 +85,87 @@ void boot()
 	wait(0.3);
 	lcd.DisplayStringAt(0, LINE(6), (uint8_t *)"#######", CENTER_MODE);
 	wait(1);
-}
-
-void start_count()
-{
-	lcd.Clear(LCD_COLOR_BLUE);
-	lcd.SetBackColor(LCD_COLOR_BLUE);
-	lcd.SetTextColor(LCD_COLOR_WHITE);
-	lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"Let the count begin!", CENTER_MODE);
-	wait(2);
 	lcd.Clear(LCD_COLOR_BLUE);
 	lcd.SetBackColor(LCD_COLOR_BLUE);
 	lcd.SetTextColor(LCD_COLOR_WHITE);
 }
 
-void counter_thread()
+void dht_thread()
 {
-	uint32_t count = 0;
-	char screen_int[32];
+	int err;
+	char temp_int[32];
+	char humi_int[32];
+	bool init = false;
 
 	while (1)
 	{
-		if (running)
+		err = sensor.readData();
+		if (err == 0)
 		{
-			count++;
-			if (count >= 9999)
-			{
-				count = 0;
-			}
-			sprintf(screen_int, "Count: %04lu", count);
-		}
-		if (!paused)
-		{
-			lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"              ", CENTER_MODE);
-			lcd.DisplayStringAt(0, LINE(5), (uint8_t *)screen_int, CENTER_MODE);
-			led2 = 1;
+			sprintf(temp_int, "Temperature: %.0f %c", sensor.ReadTemperature(corf), temp);
+			sprintf(humi_int, "Humidity: %.0f", sensor.ReadHumidity());
 
-			// Onboard LED blink every 10 number
-			if (count % 100 == 0)
-				led1 = 1;
-			else
-				led1 = 0;
-
-			running = true;
+			init = true;
 		}
 		else
 		{
-			lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"--- Paused ---", CENTER_MODE);
-			lcd.DisplayStringAt(0, LINE(5), (uint8_t *)screen_int, CENTER_MODE);
-			led2 = 0;
+			while (err != 0)
+			{
+				err = sensor.readData();
+				if (err == 0)
+				{
+					init = true;
+				}
+			}
+			sprintf(temp_int, "Temperature: %.0f %c", sensor.ReadTemperature(corf), temp);
+			sprintf(humi_int, "Humidity: %.0f", sensor.ReadHumidity());
+		}
 
-			running = false;
+		if (init)
+		{
+			lcd.DisplayStringAt(0, LINE(8), (uint8_t *)temp_int, CENTER_MODE);
+			lcd.DisplayStringAt(0, LINE(9), (uint8_t *)humi_int, CENTER_MODE);
+		}
+		else
+		{
+			lcd.DisplayStringAt(0, LINE(8), (uint8_t *)"Waiting for DHT...", CENTER_MODE);
 		}
 	}
 }
 
+void light_thread()
+{
+	while (1)
+	{
 
+		if (ls.read() > 0.3)
+		{
+			lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"Goddag", CENTER_MODE);
+		}
+		else
+		{
+			lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"Godnat", CENTER_MODE);
+		}
+	}
+}
 
 int main()
 {
+	// create a thread that'll run the event queue's dispatch function
 	Thread eventThread;
 	eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
-	
-	btn.fall(queue.event(&pause));
+
+	// wrap calls in queue.event to automatically defer to the queue's thread
+	btn.fall(queue.event(&temp_switch));
+
+	SCB_CleanDCache();
+	SCB_DisableDCache();
+
 	// Start screen
 	boot();
-	start_count();
-	counter.start(counter_thread);
+	user_input();
 
-	while (1)
-	{
-	}
+	ths.start(dht_thread);
+	light.start(light_thread);
+	wait(osWaitForever);
 }
